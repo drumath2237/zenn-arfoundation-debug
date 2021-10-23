7ba380512826511e3695c19c823d75ef1129a689
drumath <2237k.drumath@gmail.com>
2021-10-23 19:52:04 +0900

    :sparkles: 簡単なスクリプトを組んでみる

---
+++ Assets/ZennARFoundationDebug/Scripts/CubeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace ZennARFoundationDebug
+{
+    public class CubeGenerator : MonoBehaviour
+    {
+        [SerializeField] private GameObject _cube;
+
+        private Camera mainCamera;
+
+        private void Start()
+        {
+            mainCamera = Camera.main;
+        }
+
+        public void GenerateRigidBodyCube()
+        {
+            if (_cube == null)
+            {
+                return;
+            }
+
+            var transform1 = mainCamera.transform;
+            var position = transform1.position + transform1.forward * 0.1f;
+
+            Instantiate(_cube, position, quaternion.identity);
+        }
+    }
+}
